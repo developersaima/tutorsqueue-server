d60d978ef73237c125bceac4b1adb2a082a9c6ae
developersaima <saimasattar6040@gmail.com>
2026-05-21 18:06:40 +0600

    refactor: restructure database connection and add JWT verification

--- index.js
+++ index.js
@@ -1,21 +1,76 @@
+import dns from "node:dns";
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
 import dotenv from "dotenv";
 dotenv.config();
+
 import express from "express";
-import { connectDB } from "./db.js";
-import cors from "cors"
+import cors from "cors";
+import { MongoClient, ServerApiVersion, ObjectId } from "mongodb";
+import { createRemoteJWKSet, jwtVerify } from "jose";
+
 const app = express();
+app.use(cors());
+app.use(express.json());
 
-app.use(cors())
-app.use(express.json())
+const uri = process.env.MONGO_URI;
+const port = process.env.PORT || 5000;
 
-
-connectDB();
-
-
-let port = process.env.PORT || 5000;
-
-
-
-app.listen(port, () => {
-  console.log("Server is running on " + port);
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
 });
+
+const JWKS = createRemoteJWKSet(
+  new URL(`${process.env.PUBLIC_URL}/api/auth/jwks`),
+);
+
+const verifyToken = async (req, res, next) => {
+  const authHeader = req?.headers?.authorization;
+  if (!authHeader) {
+    return res.status(401).send("Unauthorized");
+  }
+  const token = authHeader.split(" ")[1];
+  if (!token) {
+    return res.status(401).send("Unauthorized");
+  }
+  try {
+    const { payload } = await jwtVerify(token, JWKS);
+    req.user = payload;
+    next();
+  } catch (error) {
+    return res.status(403).send("Forbidden");
+  }
+};
+
+async function run() {
+  try {
+    await client.connect();
+
+    const db = client.db("medi-queue");
+
+    
+    
+
+    await client.db("admin").command({ ping: 1 });
+    console.log("Connected to MongoDB!");
+  } finally {
+    // await client.close();
+  }
+}
+
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Server is running fine!");
+});
+
+if (process.env.NODE_ENV !== "production") {
+  app.listen(port, () => {
+    console.log(`Server is running on ${port}`);
+  });
+}
+
+export default app;
